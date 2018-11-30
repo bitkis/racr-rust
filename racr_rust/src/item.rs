91e3d4d9a6fd92b111eb07e6402c1d4d7166922b
Kjetil Kjeka <kjetilkjeka@gmail.com>
2018-11-30 15:28:42 +0100

    Added convinient conversion into Item

--- racr_rust/src/item.rs
+++ racr_rust/src/item.rs
@@ -27,3 +27,33 @@ impl fmt::Display for Item {
         }
     }
 }
+
+impl From<Use> for Item {
+    fn from(item: Use) -> Item {
+        Item::Use(item)
+    }
+}
+
+impl From<Module> for Item {
+    fn from(item: Module) -> Item {
+        Item::Mod(item)
+    }
+}
+
+impl From<UnitDefinition> for Item {
+    fn from(item: UnitDefinition) -> Item {
+        Item::Unit(item)
+    }
+}
+
+impl From<PeripheralDefinition> for Item {
+    fn from(item: PeripheralDefinition) -> Item {
+        Item::Peripheral(item)
+    }
+}
+
+impl From<RegisterDefinition> for Item {
+    fn from(item: RegisterDefinition) -> Item {
+        Item::Register(item)
+    }
+}
